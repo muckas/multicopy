5d59bba648fe600b564ebad736a42f865cfee3e1
muckas <motobike8@outlook.com>
2022-11-27 17:25:13 +0500

    Copying file to multiple destinations

--- main.c
+++ main.c
@@ -7,8 +7,13 @@
 #include <sys/stat.h>
 
 int main(int argc, char *argv[]) {
+	if (argc < 3) {
+		fprintf(stderr, "Usage: %s <source_file> <dest_file_1> ... <dest_file_n>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	int dest_num = argc - 2;
 	char *source_path = argv[1];
-	char *dest_path = argv[2];
 
 	int source_fd = open(source_path, O_RDONLY);
 	if (source_fd < 0) {
@@ -22,13 +27,17 @@ int main(int argc, char *argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
-	int dest_fd = open(dest_path, O_CREAT|O_WRONLY|O_TRUNC, sb.st_mode);
-	if (dest_fd < 0) {
-		fprintf(stderr, "Error writing %s: %s\n", dest_path, strerror(errno));
-		exit(EXIT_FAILURE);
+	int dest_fds[dest_num];
+	for (int i = 0; i < dest_num; i++) {
+		dest_fds[i] = open(argv[i+2], O_CREAT|O_WRONLY|O_TRUNC, sb.st_mode);
+		if (dest_fds[i] < 0) {
+			fprintf(stderr, "Error writing %s: %s\n", argv[i+2], strerror(errno));
+			exit(EXIT_FAILURE);
+		}
 	}
 
 	char buf[8192];
+	fprintf(stdout, "Copying %s to %i destinations...\n", source_path, dest_num);
 
 	while (1) {
 		ssize_t read_result = read(source_fd, &buf[0], sizeof(buf));
@@ -38,10 +47,12 @@ int main(int argc, char *argv[]) {
 		}
 		if (!read_result) break;
 
-		ssize_t write_result = write(dest_fd, &buf[0], read_result);
-		if (errno != 0) {
-		fprintf(stderr, "Error writing %s: %s\n", dest_path, strerror(errno));
-		break;
+		for (int i = 0; i < dest_num; i++) {
+			ssize_t write_result = write(dest_fds[i], &buf[0], read_result);
+			if (errno != 0) {
+			fprintf(stderr, "Error writing %s: %s\n", argv[i+2], strerror(errno));
+			break;
+			}
 		}
 	}
 	exit(EXIT_SUCCESS);
